249b8628ce37289cb12701b50044335a60e80b8e
Muhammad Rivki <Muhammad.Rivki@outlook.com>
2015-12-01 11:24:48 +0700

    Order Confirm - form

--- app/src/main/java/co/shoutnet/shoutcap/OrderConfirmation.java
+++ app/src/main/java/co/shoutnet/shoutcap/OrderConfirmation.java
@@ -3,40 +3,23 @@ package co.shoutnet.shoutcap;
 import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
 public class OrderConfirmation extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_confirmation);
 
+        toolbar = (Toolbar) findViewById(R.id.toolbar_order);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         FragmentManager fragmentManager = getFragmentManager();
         FragmentDestination destination = FragmentDestination.newInstance("dasdas", "dasdasd");
         fragmentManager.beginTransaction().add(R.id.frame_content_order, destination).commit();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_order_confirmation, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
